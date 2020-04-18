e472d5da33b49c9fcc107f1303102b3ee52ee3aa
Dima <pikus.dima25@gmail.com>
2020-04-18 16:19:43 +0300

    Добавляет моки для карточек задач(вклчая карточки редактирования)

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-
 const TASK_COUNT = 3;
 
 import {createBoardTemplate} from './components/board.js';
@@ -7,7 +6,9 @@ import {createLoadMoreButtonTemplate} from './components/load-more-button';
 import {createSiteMenuTemplate} from './components/site-menu';
 import {createTaskEditTemplate} from './components/task-edit';
 import {createTaskTemplate} from './components/task';
+
 import {generateFilters} from './mock/filter';
+import {generateTasks} from './mock/task';
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
@@ -17,6 +18,7 @@ const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 
 const filters = generateFilters();
+const tasks = generateTasks(TASK_COUNT);
 render(siteMainElement, createFilterTemplate(filters), `beforeend`);
 render(siteHeaderElement, createSiteMenuTemplate(), `beforeend`);
 render(siteMainElement, createBoardTemplate(), `beforeend`);
@@ -24,10 +26,11 @@ render(siteMainElement, createBoardTemplate(), `beforeend`);
 const taskListElement = siteMainElement.querySelector(`.board__tasks`);
 const boardElement = siteMainElement.querySelector(`.board`);
 
-render(taskListElement, createTaskEditTemplate(), `beforeend`);
+render(taskListElement, createTaskEditTemplate(tasks[0]), `beforeend`);
 
-for (let i = 0; i < TASK_COUNT; i++) {
-  render(taskListElement, createTaskTemplate(), `beforeend`);
+for (let i = 1; i < tasks.length; i++) {
+  render(taskListElement, createTaskTemplate(tasks[i]), `beforeend`);
 }
 
 render(boardElement, createLoadMoreButtonTemplate(), `beforeend`);
+
